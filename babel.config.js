30910b7dfdd0554d0fa344434b7ae5640a54f1ae
Rico Rojas <djizco@yahoo.com>
2018-09-24 23:43:13 -0500

    Use a JS babel file

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const presets = ['@babel/env', '@babel/react'];
+
+const plugins = [
+  // Stage 3
+  '@babel/plugin-proposal-class-properties',
+];
+
+module.exports = {
+  plugins,
+  presets,
+};

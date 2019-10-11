cdcf1a15f37422690cc9c2fb14079ea864de1ac7
Rico Rojas <djizco@yahoo.com>
2019-10-11 01:03:31 -0500

    Update lint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,48 @@
 module.exports = {
-  "root": true,
-  "extends": "airbnb",
-  "parserOptions": {
-    "ecmaVersion": 7,
-    "sourceType": "module",
-    "allowImportExportEverywhere": false,
+  root: true,
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module",
+    allowImportExportEverywhere: false,
   },
-  "env": {
-    "node": true,
-    "jest": true
+  env: {
+    node: true,
+    jest: true
   },
-  "rules": {
+  rules: {
     // override defaults
-    "eqeqeq": [2, "smart"],
-    "max-len": [2, {"code": 120, "tabWidth": 2, "ignoreUrls": true}],
-    "newline-per-chained-call": [2, {"ignoreChainWithDepth": 4}],
-    "no-cond-assign": [2, "except-parens"],
-    "no-multi-spaces": [2, {"exceptions": {
-      "ImportDeclaration": true,
-      "Property": true,
-      "VariableDeclarator": true,
+    eqeqeq: [2, 'smart'],
+    'max-len': [2, { 'code': 120, 'tabWidth': 2, 'ignoreUrls': true }],
+    'newline-per-chained-call': [2, { ignoreChainWithDepth: 4 }],
+    'no-cond-assign': [2, 'except-parens'],
+    'no-multi-spaces': [2, { exceptions: {
+      ImportDeclaration: true,
+      Property: true,
+      VariableDeclarator: true,
     }}],
-    "prefer-destructuring": [2, {
-      "array": false,
-      "object": true
+    'prefer-destructuring': [2, {
+      array: false,
+      object: true
     }, {
-      "enforceForRenamedProperties": false
+      enforceForRenamedProperties: false
     }],
-    "space-before-function-paren": [2, "never"],
+    'space-before-function-paren': [2, 'never'],
 
     // disable defaults
-    "arrow-parens": 0,
-    "brace-style": 0,
-    "consistent-return": 0,
-    "func-names": 0,
-    "implicit-arrow-linebreak": 0,
-    "no-confusing-arrow": 0,
-    "no-console": 0,
-    "no-nested-ternary": 0,
-    "no-param-reassign": 0,
-    "no-plusplus": 0,
-    "no-shadow": 0,
-    "no-underscore-dangle": 0,
-    "object-curly-newline": 0,
-    "padded-blocks": 0,
+    'arrow-parens': 0,
+    'brace-style': 0,
+    'consistent-return': 0,
+    'func-names': 0,
+    'implicit-arrow-linebreak': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 0,
+    'no-nested-ternary': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': 0,
+    'no-shadow': 0,
+    'no-underscore-dangle': 0,
+    'object-curly-newline': 0,
+    'padded-blocks': 0,
   },
 };
--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,63 +1,65 @@
 module.exports = {
-  "settings": {
-    "import/resolver": {
-      "webpack": {
-        "config": './client/config/webpack.config.js',
+  root: true,
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './client/config/webpack.config.js',
       }
     },
   },
-  "extends": "airbnb",
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 7,
-    "sourceType": "module",
-    "allowImportExportEverywhere": false,
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
   },
-  "env": {
-    "browser": true,
-    "jest": true,
+  env: {
+    browser: true,
+    jest: true,
   },
-  "plugins": ["react"],
-  "rules": {
+  plugins: ['react', 'react-hooks'],
+  rules: {
     // enable rules
-    "react/jsx-key": 2,
+    'react/jsx-key': 2,
+    'react-hooks/rules-of-hooks': 2,
 
     // override default options for rules from base configurations
-    "eqeqeq": [2, "smart"],
-    "max-len": [2, {"code": 120, "tabWidth": 2, "ignoreUrls": true}],
-    "newline-per-chained-call": [2, {"ignoreChainWithDepth": 4}],
-    "no-cond-assign": [2, "except-parens"],
-    "no-multi-spaces": [2, {"exceptions": {
-      "ImportDeclaration": true,
-      "Property": true,
-      "VariableDeclarator": true
+    'eqeqeq': [2, 'smart'],
+    'max-len': [2, {'code': 120, 'tabWidth': 2, 'ignoreUrls': true}],
+    'newline-per-chained-call': [2, {'ignoreChainWithDepth': 4}],
+    'no-cond-assign': [2, 'except-parens'],
+    'no-multi-spaces': [2, {'exceptions': {
+      'ImportDeclaration': true,
+      'Property': true,
+      'VariableDeclarator': true
     }}],
-    "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx"] }],
-    "react/jsx-fragments": [2, 'element'],
+    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx'] }],
+    'react/jsx-fragments': [2, 'element'],
 
     // disable rules
-    "arrow-parens": 0,
-    "function-paren-newline": 0,
-    "implicit-arrow-linebreak": 0,
-    "import/no-cycle": 0,
-    "import/no-extraneous-dependencies": 0,
-    "import/no-named-as-default": 0,
-    "import/prefer-default-export": 0,
-    "jsx-a11y/anchor-is-valid": 0,
-    "jsx-a11y/label-has-associated-control": 0,
-    "jsx-a11y/label-has-for": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
-    "no-confusing-arrow": 0,
-    "no-nested-ternary": 0,
-    "no-plusplus": 0,
-    "no-shadow": 0,
-    "no-underscore-dangle": 0,
-    "object-curly-newline": 0,
-    "react/destructuring-assignment": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-boolean-value": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/state-in-constructor": 0,
-    "react/static-property-placement": 0,
+    'arrow-parens': 0,
+    'function-paren-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'import/no-cycle': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/prefer-default-export': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'no-confusing-arrow': 0,
+    'no-nested-ternary': 0,
+    'no-plusplus': 0,
+    'no-shadow': 0,
+    'no-underscore-dangle': 0,
+    'object-curly-newline': 0,
+    'react/destructuring-assignment': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-boolean-value': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/state-in-constructor': 0,
+    'react/static-property-placement': 0,
   },
 };

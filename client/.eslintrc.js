712753ece86c5f2f95e4057ec1b779c63c7dce27
Rico Rojas <djizco@yahoo.com>
2021-07-14 23:52:36 -0500

    Update packages, downgrade react-notifications-component

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
     // override default options for rules from base configurations
     'eqeqeq': [2, 'smart'],
     'max-len': [2, { 'code': 120, 'tabWidth': 2, 'ignoreUrls': true }],
-    'newline-per-chained-call': [2, {'ignoreChainWithDepth': 4}],
+    'newline-per-chained-call': [2, { 'ignoreChainWithDepth': 4 }],
     'no-cond-assign': [2, 'except-parens'],
     'no-multi-spaces': [2, {'exceptions': {
       'ImportDeclaration': true,

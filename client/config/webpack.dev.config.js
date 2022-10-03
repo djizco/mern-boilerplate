d498759f4075cfb959f200cdc0cd0ae6d75046fc
Rico Rojas <djizco@yahoo.com>
2022-10-02 23:47:26 -0500

    Add eslint webpack plugin. Update packages

--- client/config/webpack.dev.config.js
+++ client/config/webpack.dev.config.js
@@ -1,3 +1,9 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 const config = require('./webpack.config');
+
+const ESLintPluginConfig = new ESLintPlugin();
+
+config.plugins.push(ESLintPluginConfig);
 
 module.exports = config;
